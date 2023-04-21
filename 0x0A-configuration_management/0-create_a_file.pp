344dc4711030bdd1a157ffd6ade4a447e524f007
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 <stevenviko@gmail.com>
2023-04-20 23:50:01 -0700

    Task 0. Create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# Creating a file in /tmp/school
+
+file { '/tmp/school':
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
